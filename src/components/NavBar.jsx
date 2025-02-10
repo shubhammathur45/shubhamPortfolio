1db952fb09dbc3a25319c037e526fb3fe2769347
shubhammathur45 <shubham.mathur0808@gmail.com>
2025-02-10 13:51:36 -0500

    Fixing scroll issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import {Link} from 'react-scroll';
 
 function NavBar() {
     const [nav, setNav] = useState(false);
@@ -40,8 +41,9 @@ function NavBar() {
                 {links.map(({ id, link }) => (
                     <li key={id}
                         className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
-                        {link}
+                        <Link to={link} smooth duration={500}>{link}</Link>
                     </li>
+                    
                 ))}
 
             </ul>
